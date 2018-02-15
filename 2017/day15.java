9c4131bcc93ef6f40b14900eb1fcab2bdd5fd6b8
Namdrib <denton_the_bird_man@hotmail.com>
2018-02-15 23:34:31 +1030

    Added day15 part1, 2, day16 part1

---
+++ 2017/day15.java
@@ -0,0 +1,109 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Scanner;
+
+// https://adventofcode.com/2017/day/15
+
+public class day15
+{
+	public class Generator
+	{
+		final long			factor;
+		final static long	divisor	= 2147483647;
+		final long			multiple;
+		final boolean		partTwo;
+		long				start;
+
+		Generator(long factor, long multiple, long start, boolean partTwo)
+		{
+			this.factor = factor;
+			this.multiple = multiple;
+			this.start = start;
+			this.partTwo = partTwo;
+		}
+
+		public void init(long start)
+		{
+			this.start = start;
+		}
+
+		private void updateStart()
+		{
+			start = (start * factor) % divisor;
+		}
+
+		public long getNextNumber()
+		{
+			updateStart();
+
+			if (partTwo == true)
+			{
+				while (start % multiple != 0)
+				{
+					updateStart();
+				}
+			}
+
+			return start;
+		}
+	}
+
+	public day15()
+	{
+		;
+	}
+
+	int samePairs(long startA, long startB, int count, boolean partTwo)
+	{
+		Generator a = new Generator(16807, 4, startA, partTwo);
+		Generator b = new Generator(48271, 8, startB, partTwo);
+		int numSamePairs = 0;
+
+		while (count-- > 0)
+		{
+			long numA = a.getNextNumber();
+			long numB = b.getNextNumber();
+
+			long significantA = numA & 0xFFFF;
+			long significantB = numB & 0xFFFF;
+			if (significantA == significantB)
+			{
+				numSamePairs++;
+			}
+		}
+
+		return numSamePairs;
+	}
+
+	public static void main(String[] args)
+	{
+		day15 a = new day15();
+		if (args.length > 0)
+		{
+			assertEquals(1, a.samePairs(65, 8921, 5, false));
+			assertEquals(588, a.samePairs(65, 8921, 40000000, false));
+
+			assertEquals(0, a.samePairs(65, 8921, 5, true));
+			assertEquals(0, a.samePairs(65, 8921, 1055, true));
+			assertEquals(1, a.samePairs(65, 8921, 1056, true));
+			assertEquals(309, a.samePairs(65, 8921, 5000000, true));
+
+			System.out.println("Tests successful!");
+			return;
+		}
+
+		long startA, startB;
+		try (Scanner scanner = new Scanner(System.in))
+		{
+			String lineA = scanner.nextLine();
+			String[] elementsA = lineA.split(" ");
+			startA = Long.parseLong(elementsA[elementsA.length - 1]);
+			String lineB = scanner.nextLine();
+			String[] elementsB = lineB.split(" ");
+			startB = Long.parseLong(elementsB[elementsB.length - 1]);
+		}
+
+		System.out.println("Part 1: " + a.samePairs(startA, startB, 40000000, false));
+		System.out.println("Part 2: " + a.samePairs(startA, startB, 5000000, true));
+	}
+}
---
+++ 2017/day16.java
@@ -0,0 +1,109 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+// https://adventofcode.com/2017/day/16
+
+public class day16
+{
+
+	public day16()
+	{
+		;
+	}
+	
+	public String dance(int numPrograms, List<String> moves, long times)
+	{
+		// Build the line of programs
+		String line = new String();
+		for (char c = 'a'; c - 'a' < numPrograms; c++)
+		{
+			line += c;
+		}
+		
+		for (long i=0; i<times; i++)
+		{
+			if (i%1000000 == 0) System.out.println("Set "+ i);
+			for (String move : moves)
+			{
+				line = performMove(line, move);
+			}
+		}
+		
+		return line;
+	}
+	
+	private String performMove(String line, String move)
+	{
+		String temp = move.substring(1);
+		char arr[] = line.toCharArray();
+		switch (move.charAt(0))
+		{
+			case 's':
+			{
+				int amount = Integer.parseInt(temp);
+				// Rotate arr to the right by temp, then put back into line
+				String end = line.substring(0, line.length() - amount);
+				String begin = line.substring(line.length() - amount);
+				arr = (begin + end).toCharArray();
+				break;
+			}
+			case 'x':
+			{
+				String[] parts = temp.split("/");
+				int a = Integer.parseInt(parts[0]);
+				int b = Integer.parseInt(parts[1]);
+				char swap = arr[a];
+				arr[a] = arr[b];
+				arr[b] = swap;
+				break;
+			}
+			case 'p':
+			{
+				char a = move.charAt(1);
+				char b = move.charAt(3);
+				
+				int aPos = line.indexOf(a);
+				int bPos = line.indexOf(b);
+				char swap = arr[aPos];
+				arr[aPos] = arr[bPos];
+				arr[bPos] = swap;
+				break;
+			}
+			default:
+				System.err.println("Error, first char: " + line.charAt(0));
+		}
+		return new String(arr);
+	}
+
+	public static void main(String[] args)
+	{
+		day16 a = new day16();
+		if (args.length > 0)
+		{
+			assertEquals("baedc", a.dance(5, Arrays.asList("s1", "x3/4", "pe/b"), 1));
+			assertEquals("ceadb", a.dance(5, Arrays.asList("s1", "x3/4", "pe/b"), 2));
+
+			System.out.println("Tests successful!");
+			return;
+		}
+		
+		List<String> input = new ArrayList<>();
+		
+		// Take input, turn into usable form
+		try (Scanner scanner = new Scanner(System.in))
+		{
+			String[] moves = scanner.nextLine().split(",");
+			for (String s : moves)
+			{
+				input.add(s);
+			}
+		}
+
+		System.out.println("Part 1: " + a.dance(16, input, 1));
+		System.out.println("Part 1: " + a.dance(16, input, 1000000000));
+	}
+}
