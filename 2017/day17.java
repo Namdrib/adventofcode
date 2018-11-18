147e5844aa8c21461ce9aa9a109ad777a7656ab3
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-11-18 23:17:35 +1030

    merge day17 part 1 and part 2 into solve()
    it takes a boolean arg 'partTwo' to select part 1 or part 2 solution
    the reason for this is the heavy code repetition in parts 1 and 2

--- 2017/day17.java
+++ 2017/day17.java
@@ -14,78 +14,83 @@ public class day17
 		;
 	}
 
-	public int partOne(int numInsertions, int spinsPerInsert, int query)
+	/**
+	 * Return the value after `target` after `numInsertions` insertions at `spinsPerInsert` spins per
+	 * insertion
+	 * 
+	 * <b>Part One</b>
+	 * 
+	 * Create a simulation, populate a buffer with the insertions and spins. This is required because
+	 * something might be inserted <i>behind</i> <code>target</code>
+	 * 
+	 * <b>Part Two</b>
+	 * 
+	 * Simulating the buffer takes way too long, so need to work it out w/o the buffer. Only keep
+	 * track of the number at position <code>target+1</code>. Therefore also need to track the index
+	 * at which <code>target</code> was inserted
+	 * 
+	 * @param target some number in the buffer
+	 * @param numInsertions number of times to perform an insertion into the buffer
+	 * @param spinsPerInsert how many times the buffer advances before an insertion is made
+	 * @param partTwo whether we are solving for part two
+	 * @return the value after `target` after `numInsertions` insertions
+	 */
+	public int solve(int target, int numInsertions, int spinsPerInsert, boolean partTwo)
 	{
 		List<Integer> buffer = new ArrayList<>();
 		buffer.add(0);
-		int position = 0;
-		int counter = 1; // number being inserted
+		int currentIndex = 0;
+		int numToInsert = 1;
+		int bufferSize = 1; // (starts with zero in the buffer)
+
+		int targetIndex = 0; // index where target is inserted
+		int valueAfterTarget = 0; // return value
 
 		// spin the buffer
 		// perform numInsertions on buffer, spinning spinsPerInsert on each insert
 		for (int i = 0; i < numInsertions; i++)
 		{
 			// perform spins
-			position += spinsPerInsert;
-			position %= buffer.size();
+			currentIndex += spinsPerInsert;
+			currentIndex %= bufferSize;
 
 			// insert the number
-			position++;
-			buffer.add(position, counter);
-			counter++;
-		}
-
-		// return the value after `query`
-		int indexOfQuery = buffer.indexOf(query);
-		int indexAfter = (indexOfQuery + 1) % buffer.size();
-		return buffer.get(indexAfter);
-	}
-
-	// return the value after `target` after `numInsertions` insertions
-	// at `spinsPerInsert` spins per insertion
-	// simulating the buffer takes way too long
-	// so we work it out without the buffer
-	// only need to keep track of the number at the position `target+1`
-	public int partTwo(int target, int numInsertions, int spinsPerInsert)
-	{
-		int position = 0;
-		int counter = 1; // number being inserted
-		int size = 1; // size of the buffer
-
-		int valueAfterTarget = 0;
-
-		// make a half-arsed simulation without manipulating a buffer
-		// just use information I know like length, insertions, spins, etc.
-		for (int i = 0; i < numInsertions; i++)
-		{
-			// perform spins
-			position += spinsPerInsert;
-			position %= size;
-
-			// "insert" the number
-			// if we are inserting directly after `target`, record it
-			position++;
-			if (position == (target + 1) % size)
+			currentIndex++;
+			if (partTwo)
 			{
-				valueAfterTarget = counter;
+				if (currentIndex == (targetIndex + 1) % bufferSize)
+				{
+					valueAfterTarget = numToInsert;
+				}
 			}
-			size++;
-			counter++;
+			else
+			{
+				buffer.add(currentIndex, numToInsert);
+			}
+			bufferSize++;
+			numToInsert++;
 		}
 
-		return valueAfterTarget;
+		// return the value after `target`
+		if (partTwo)
+		{
+			return valueAfterTarget;
+		}
+		else
+		{
+			targetIndex = buffer.indexOf(target);
+			int indexAfter = (targetIndex + 1) % bufferSize;
+			return buffer.get(indexAfter);
+		}
 	}
 
 	public static void main(String[] args)
 	{
 		day17 a = new day17();
-		int numInsertions = 2017;
-		List<Integer> buffer = new ArrayList<>();
-		buffer.add(0);
 
 		if (args.length > 0)
 		{
-			assertEquals(638, a.partOne(numInsertions, 3, numInsertions));
+			assertEquals(638, a.solve(2017, 2017, 3, false));
 
 			System.out.println("Tests successful!");
 			return;
@@ -97,7 +102,7 @@ public class day17
 			spinsPerInsert = scanner.nextInt();
 		}
 
-		System.out.println("Part 1: " + a.partOne(numInsertions, spinsPerInsert, numInsertions));
-		System.out.println("Part 2: " + a.partTwo(0, 50_000_000, spinsPerInsert));
+		System.out.println("Part 1: " + a.solve(2017, 2017, spinsPerInsert, false));
+		System.out.println("Part 2: " + a.solve(0, 50_000_000, spinsPerInsert, true));
 	}
 }
