b537e9b76775c92d4292fc13b7e3ddfffe78e45a
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-04 00:23:08 +1030

    #3 reformat existing java code

--- src/main/java/_2017/day05.java
+++ src/main/java/_2017/day05.java
@@ -8,64 +8,53 @@ import java.util.Scanner;
 
 // http://adventofcode.com/2017/day/5
 
-public class day05
-{
-	public day05()
-	{
-		;
-	}
+public class day05 {
+  public day05() {
+    ;
+  }
 
-	public int numMovesToEscape(List<Integer> maze, boolean partTwo)
-	{
-		int numMoves = 0;
+  public int numMovesToEscape(List<Integer> maze, boolean partTwo) {
+    int numMoves = 0;
 
-		int position = 0;
+    int position = 0;
 
-		while (position >= 0 && position < maze.size())
-		{
-			int oldPosition = position;
-			position += maze.get(position);
-			if (partTwo && maze.get(oldPosition) >= 3)
-			{
-				maze.set(oldPosition, maze.get(oldPosition) - 1);
-			}
-			else
-			{
-				maze.set(oldPosition, maze.get(oldPosition) + 1);
-			}
-			numMoves++;
-		}
+    while (position >= 0 && position < maze.size()) {
+      int oldPosition = position;
+      position += maze.get(position);
+      if (partTwo && maze.get(oldPosition) >= 3) {
+        maze.set(oldPosition, maze.get(oldPosition) - 1);
+      } else {
+        maze.set(oldPosition, maze.get(oldPosition) + 1);
+      }
+      numMoves++;
+    }
 
-		return numMoves;
-	}
+    return numMoves;
+  }
 
-	public static void main(String[] args)
-	{
-		day05 a = new day05();
+  public static void main(String[] args) {
+    day05 a = new day05();
 
-		if (args.length > 0)
-		{
-			List<Integer> input = Arrays.asList(0, 3, 0, 1, -3);
-			assertEquals(5, a.numMovesToEscape(new ArrayList<>(input), false));
-			assertEquals(10, a.numMovesToEscape(new ArrayList<>(input), true));
+    if (args.length > 0) {
+      List<Integer> input = Arrays.asList(0, 3, 0, 1, -3);
+      assertEquals(5, a.numMovesToEscape(new ArrayList<>(input), false));
+      assertEquals(10, a.numMovesToEscape(new ArrayList<>(input), true));
 
-			System.out.println("Tests successful!");
-			return;
-		}
+      System.out.println("Tests successful!");
+      return;
+    }
 
-		List<Integer> input = new ArrayList<Integer>();
+    List<Integer> input = new ArrayList<Integer>();
 
-		// Take input, turn into usable form
-		try (Scanner scanner = new Scanner(System.in))
-		{
-			while (scanner.hasNextInt())
-			{
-				int temp = scanner.nextInt();
-				input.add(temp);
-			}
-		}
+    // Take input, turn into usable form
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (scanner.hasNextInt()) {
+        int temp = scanner.nextInt();
+        input.add(temp);
+      }
+    }
 
-		System.out.println("Part 1: " + a.numMovesToEscape(new ArrayList<>(input), false));
-		System.out.println("Part 2: " + a.numMovesToEscape(new ArrayList<>(input), true));
-	}
+    System.out.println("Part 1: " + a.numMovesToEscape(new ArrayList<>(input), false));
+    System.out.println("Part 2: " + a.numMovesToEscape(new ArrayList<>(input), true));
+  }
 }
--- src/main/java/_2017/day15.java
+++ src/main/java/_2017/day15.java
@@ -5,106 +5,91 @@ import java.util.Scanner;
 
 // https://adventofcode.com/2017/day/15
 
-public class day15
-{
-	public class Generator
-	{
-		final long factor;
-		final static long divisor = 2147483647;
-		final long multiple;
-		final boolean partTwo;
-		long start;
+public class day15 {
+  public class Generator {
+    final long factor;
+    final static long divisor = 2147483647;
+    final long multiple;
+    final boolean partTwo;
+    long start;
 
-		Generator(long factor, long multiple, long start, boolean partTwo)
-		{
-			this.factor = factor;
-			this.multiple = multiple;
-			this.start = start;
-			this.partTwo = partTwo;
-		}
+    Generator(long factor, long multiple, long start, boolean partTwo) {
+      this.factor = factor;
+      this.multiple = multiple;
+      this.start = start;
+      this.partTwo = partTwo;
+    }
 
-		public void init(long start)
-		{
-			this.start = start;
-		}
+    public void init(long start) {
+      this.start = start;
+    }
 
-		private void updateStart()
-		{
-			start = (start * factor) % divisor;
-		}
+    private void updateStart() {
+      start = (start * factor) % divisor;
+    }
 
-		public long getNextNumber()
-		{
-			updateStart();
+    public long getNextNumber() {
+      updateStart();
 
-			if (partTwo == true)
-			{
-				while (start % multiple != 0)
-				{
-					updateStart();
-				}
-			}
+      if (partTwo == true) {
+        while (start % multiple != 0) {
+          updateStart();
+        }
+      }
 
-			return start;
-		}
-	}
+      return start;
+    }
+  }
 
-	public day15()
-	{
-		;
-	}
+  public day15() {
+    ;
+  }
 
-	int samePairs(long startA, long startB, int count, boolean partTwo)
-	{
-		Generator a = new Generator(16807, 4, startA, partTwo);
-		Generator b = new Generator(48271, 8, startB, partTwo);
-		int numSamePairs = 0;
+  int samePairs(long startA, long startB, int count, boolean partTwo) {
+    Generator a = new Generator(16807, 4, startA, partTwo);
+    Generator b = new Generator(48271, 8, startB, partTwo);
+    int numSamePairs = 0;
 
-		while (count-- > 0)
-		{
-			long numA = a.getNextNumber();
-			long numB = b.getNextNumber();
+    while (count-- > 0) {
+      long numA = a.getNextNumber();
+      long numB = b.getNextNumber();
 
-			long significantA = numA & 0xFFFF;
-			long significantB = numB & 0xFFFF;
-			if (significantA == significantB)
-			{
-				numSamePairs++;
-			}
-		}
+      long significantA = numA & 0xFFFF;
+      long significantB = numB & 0xFFFF;
+      if (significantA == significantB) {
+        numSamePairs++;
+      }
+    }
 
-		return numSamePairs;
-	}
+    return numSamePairs;
+  }
 
-	public static void main(String[] args)
-	{
-		day15 a = new day15();
-		if (args.length > 0)
-		{
-			assertEquals(1, a.samePairs(65, 8921, 5, false));
-			assertEquals(588, a.samePairs(65, 8921, 40000000, false));
+  public static void main(String[] args) {
+    day15 a = new day15();
+    if (args.length > 0) {
+      assertEquals(1, a.samePairs(65, 8921, 5, false));
+      assertEquals(588, a.samePairs(65, 8921, 40000000, false));
 
-			assertEquals(0, a.samePairs(65, 8921, 5, true));
-			assertEquals(0, a.samePairs(65, 8921, 1055, true));
-			assertEquals(1, a.samePairs(65, 8921, 1056, true));
-			assertEquals(309, a.samePairs(65, 8921, 5000000, true));
+      assertEquals(0, a.samePairs(65, 8921, 5, true));
+      assertEquals(0, a.samePairs(65, 8921, 1055, true));
+      assertEquals(1, a.samePairs(65, 8921, 1056, true));
+      assertEquals(309, a.samePairs(65, 8921, 5000000, true));
 
-			System.out.println("Tests successful!");
-			return;
-		}
+      System.out.println("Tests successful!");
+      return;
+    }
 
-		long startA, startB;
-		try (Scanner scanner = new Scanner(System.in))
-		{
-			String lineA = scanner.nextLine();
-			String[] elementsA = lineA.split(" ");
-			startA = Long.parseLong(elementsA[elementsA.length - 1]);
-			String lineB = scanner.nextLine();
-			String[] elementsB = lineB.split(" ");
-			startB = Long.parseLong(elementsB[elementsB.length - 1]);
-		}
+    long startA, startB;
+    try (Scanner scanner = new Scanner(System.in)) {
+      String lineA = scanner.nextLine();
+      String[] elementsA = lineA.split(" ");
+      startA = Long.parseLong(elementsA[elementsA.length - 1]);
+      String lineB = scanner.nextLine();
+      String[] elementsB = lineB.split(" ");
+      startB = Long.parseLong(elementsB[elementsB.length - 1]);
+    }
 
-		System.out.println("Part 1: " + a.samePairs(startA, startB, 40000000, false));
-		System.out.println("Part 2: " + a.samePairs(startA, startB, 5000000, true));
-	}
+    System.out.println("Part 1: " + a.samePairs(startA, startB, 40000000, false));
+    System.out.println("Part 2: " + a.samePairs(startA, startB, 5000000, true));
+  }
 }
