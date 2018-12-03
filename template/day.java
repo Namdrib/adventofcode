a8d21c4463f0927e2aa4a86d514b84643efe07da
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-03 17:25:47 +1030

    #1 add jdocs to template day

--- template/day.java
+++ template/day.java
@@ -3,16 +3,37 @@ package template;
 import static org.junit.Assert.assertEquals;
 import java.io.*;
 import java.util.*;
+import util.Util;
 
 // https://adventofcode.com/YEAR/day/DAY
 
 public interface day
 {
+	/**
+	 * solve and partX can treat each other like wrapper functions
+	 * 
+	 * sometimes they can both be under the same function with an if/else
+	 * to determine switching logic between the two parts rather than
+	 * copy/pasting entire functions of logic
+	 * 
+	 * @param input
+	 * @param partTwo whether to solve for partTwo
+	 * @return
+	 */
+	public String solve(List<String> input, boolean partTwo);
 
-	public <E> String solve(List<E> input, boolean partTwo);
-
+	/**
+	 * Return the solution to a day's part 1
+	 * @param input
+	 * @return the solution to the day's part 1
+	 */
 	public String partOne(List<String> input);
 
+	/**
+	 * Return the solution to a day's part 2
+	 * @param input
+	 * @return the solution to the day's part 2
+	 */
 	public String partTwo(List<String> input);
 
 	public static void main(String[] args)
@@ -30,18 +51,12 @@ public interface day
 		}
 
 		// Take input, turn into usable form
-		String input = null;
-		try (Scanner scanner = new Scanner(System.in))
-		{
-			if (scanner.hasNextLine())
-			{
-				input = scanner.next();
-			}
-		}
+		String filename = "_YEAR/dayDAY_01.in";
+		List<String> input = Util.readFileIntoListString(filename);
 
 		// Do something with the input and a
 
-		// System.out.println("Part 1: " + a.partOne());
-		// System.out.println("Part 2: " + a.partTwo());
+		// System.out.println("Part 1: " + a.partOne(input));
+		// System.out.println("Part 2: " + a.partTwo(input));
 	}
 }
