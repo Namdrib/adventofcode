4e5d549c1e9b1b185b8355184940be58d758eb8b
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-04 21:22:49 +1030

    remove sleepAmount from 2018/day04

--- src/main/java/_2018/day04.java
+++ src/main/java/_2018/day04.java
@@ -7,32 +7,25 @@ import template.day;
 import util.Global;
 import util.Util;
 
+// TODO : stream it up
+
 public class day04 extends day {
 
   // <ID, <sleepMinute, Count>>
   Map<Integer, Map<Integer, Integer>> sleepCounts = new HashMap<>();
 
-  // <ID, numTimesAsleep>
-  Map<Integer, Integer> sleepAmount = new HashMap<>();
-
   public void presolve(List<String> input) {
 
     Collections.sort(input);
     sleepCounts = new HashMap<>();
-    sleepAmount = new HashMap<>();
 
     int id = 0; // guard's id
     int pMinute = 0; // previous minute (for looping)
     for (String s : input) {
       String[] tokens = s.split("]");
       String[] datetimes = tokens[0].split(" ");
-      // String[] dates = datetimes[0].split("-");
-      // int year = Integer.parseInt(dates[0].substring(1, dates[0].length()));
-      // int month = Integer.parseInt(dates[1]);
-      // int day = Integer.parseInt(dates[2]);
 
       String[] times = datetimes[1].split(":");
-      // int hour = Integer.parseInt(times[0]);
       int minute = Integer.parseInt(times[1]);
 
       String[] actions = tokens[1].split(" ");
@@ -49,8 +42,6 @@ public class day04 extends day {
             break;
 
           case "wakes":
-            sleepAmount.merge(id, 1, Integer::sum); // sleepAmount[id]++;
-
             for (int i = pMinute; i < minute; i++) {
               // sleepCounts[id][p]++;
               // 3verbose5me
@@ -103,12 +94,6 @@ public class day04 extends day {
 
     // for that guard, find the minute they are most likely to be asleep
     // i.e. sleepCounts<maxSleepGuardID> with maximum key
-    int mostFreqSleepAmount = -1;
-    int mostFreqSleepMinute = -1;
-    for (Entry<Integer, Integer> gsa : sleepCounts.get(maxSleepGuardID).entrySet()) {
-
-    }
-
     int mostSleepMinute = Collections
         .max(sleepCounts.get(maxSleepGuardID).entrySet(), (e1, e2) -> e1.getValue() - e2.getValue())
         .getKey();
