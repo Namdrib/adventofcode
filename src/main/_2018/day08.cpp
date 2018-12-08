1c41af083813fd4063bfc2751cb539421c0f75ec
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-08 17:12:16 +1030

    refactor 2018/day08
    previous sol didn't store any values so
    part 2 became rather difficult. now, there is
    a pass over the data to structure it
    then part 1 and part 2 can be done separately

--- src/main/_2018/day08.cpp
+++ src/main/_2018/day08.cpp
@@ -4,38 +4,93 @@ using namespace std;
 
 // http://adventofcode.com/2018/day/8
 
-int recurse(const vector<int> &input, int start, int &metadata_sum, int &value_sum, int depth) {
+static int id = 0;
 
+class node
+{
+public:
+	vector<node> children;
+	vector<int> metadata;
+
+	int id;
+};
+
+
+void read_recurse(const vector<int> &input, node &root, int &start) {
 	int num_children = input[start];
 	int num_metadata = input[start+1];
 
-	// cout << "Node " << depth << " has " << num_children << " children, " << num_metadata << " metadata" << endl;
+	root.children.resize(num_children);
+	root.metadata.resize(num_metadata);
+	root.id = id;
+	id++;
 
-	while (num_children--) {
-		start = recurse(input, start+2, metadata_sum, value_sum, depth+1);
+	// cout << char('A' + root.id) << " starts at " << start << ", has " << num_children << " children, " << num_metadata << " metadata" << endl;
+
+	for (auto &child : root.children) {
+		start += 2;
+		read_recurse(input, child, start);
 	}
 
-	for (int i=start + 2; i < start + num_metadata + 2; i++) {
-		// cout << "metadata for node " << depth << ": " << input[i] << endl;
-		metadata_sum += input[i];
+	for (auto &metadata : root.metadata) {
+		start++;
+		metadata = input[start+1];
 	}
-
-	return start + num_metadata;
+	// cout << "metadata for " << char('A' + root.id) << " is " << root.metadata << endl;
 }
 
-// use bfs to solve dependencies
-int solve(vector<int> input, bool part_two) {
-	int metadata = 0;
-	int value = 0;
+// recursively sum the metadata of a root and its children
+int sum_metadata(const node &root) {
 
-	recurse(input, 0, metadata, value, 0);
+	int out = 0;
 
-	return (part_two) ? value : metadata;
+	for (auto child : root.children) {
+		out += sum_metadata(child);
+	}
+	out += accumulate(all(root.metadata), 0);
+
+	return out;
+}
+
+// recursively sum the _value_ of a root and its children
+// a node's value is the sum of its metadata (if no children)
+// or the value of its children, as referred to by the metadata
+// children may be counted multiple times
+int sum_value(const node &root) {
+
+	int out = 0;
+
+	if (root.children.empty()) {
+		return sum_metadata(root);
+	}
+
+	// metadata are 1-indexed
+	for (auto metadata : root.metadata) {
+		if (metadata > 0 && metadata <= root.children.size()) {
+			out += sum_value(root.children[metadata-1]);
+		}
+	}
+
+	return out;
+
+}
+
+int solve(node root, bool part_two) {
+
+	if (part_two) {
+		return sum_value(root);
+	}
+
+	return sum_metadata(root);
 }
 
 int main() {
 	vector<int> input = split_istream_by_whitespace<int>(cin);
 
-	cout << "Part 1: " << solve(input, false) << endl;
-	// cout << "Part 2: " << solve(input, true) << endl;
+	node root;
+	int start = 0;
+	read_recurse(input, root, start);
+
+	cout << "Part 1: " << solve(root, false) << endl;
+	cout << "Part 2: " << solve(root, true) << endl;
 }
