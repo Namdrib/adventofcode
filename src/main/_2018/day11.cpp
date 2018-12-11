e691f5aae6e521b6cff83fc0abecf540d8588867
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-11 18:59:59 +1030

    optimise 2018/day11 part 2 using partial sums

--- src/main/_2018/day11.cpp
+++ src/main/_2018/day11.cpp
@@ -4,48 +4,47 @@ using namespace std;
 
 // http://adventofcode.com/2018/day/11
 
-long get_power(int x, int y, int grid_serial_number) {
-	long rack = x + 10;
-	long power = rack * y;
+int get_power(int x, int y, int grid_serial_number) {
+	int rack = x + 10;
+	int power = rack * y;
 	power += grid_serial_number;
 	power *= rack;
-	long hundreds = (power / 100) % 10;
-	return hundreds-5;
+	int hundreds = (power / 100) % 10;
+	return hundreds - 5;
 }
 
 // No output value
 // Manually determine the correct output
 string solve(int input, bool part_two) {
 
-	vector<vector<int>> grid(300, vector<int>(300, -1));
+	// build 2d partial sums
+	// row and column 0 are blank
+	// remember to offset when doing calculations
+	vector<vector<long>> grid(301, vector<long>(301, 0));
+
+	for (size_t i = 1; i < grid.size(); i++) {
+		for (size_t j = 1; j < grid.size(); j++) {
+			int power = get_power(j, i, input);
+			grid[i][j] = power + grid[i][j-1] + grid[i-1][j] - grid[i-1][j-1];
+		}
+	}
 
 	long max_power = numeric_limits<long>::min();
 	size_t max_power_size = 1;
 	pair<size_t, size_t> coords;
 
-	for (size_t size = 1; size <= grid.size(); size++) {
+	size_t start = (part_two ? 1 : 3);
+	size_t end = (part_two ? grid.size()-1 : 3);
+	for (size_t size = start; size <=end; size++) {
+		for (size_t i = size; i < grid.size(); i++) {
+			for (size_t j = size; j < grid[i].size(); j++) {
 
-		// for each starting position
-		for (size_t i=0; i<grid.size()-size; i++) {
-			for (size_t j=0; j<grid[i].size()-size; j++) {
+				long sum = grid[i][j] - grid[i][j-size] - grid[i-size][j] + grid[i-size][j-size];
 
-				// calculate the sum of the square starting at [i][j]
-				long sum = 0;
-				for (size_t y = 0; y < size; y++) {
-					for (size_t x = 0; x < size; x++) {
-						size_t destx = j + x;
-						size_t desty = i + y;
-						int *dest = &(grid[desty][destx]);
-						if (*dest == -1) {
-							*dest = get_power(j+x, i+y, input);
-						}
-						sum += *dest;
-					}
-				}
 				if (sum > max_power) {
 					max_power = sum;
-					coords = make_pair(j, i);
 					max_power_size = size;
+					coords = make_pair(j - size + 1, i - size + 1);
 				}
 			}
 		}
@@ -56,7 +55,6 @@ string solve(int input, bool part_two) {
 	if (part_two) {
 		ss << "," << max_power_size;
 	}
-	cout << "returning " << ss.str() << endl;
 	return ss.str();
 }
 
@@ -64,15 +62,16 @@ int main() {
 	int input;
 	cin >> input;
 
-	// assert(get_power(3, 5, 8) == 4);
-	// assert(get_power(122, 79, 57) == -5);
-	// assert(get_power(217, 196, 39) == 0);
-	// assert(get_power(101, 153, 71) == 4);
+	assert(get_power(3, 5, 8) == 4);
+	assert(get_power(122, 79, 57) == -5);
+	assert(get_power(217, 196, 39) == 0);
+	assert(get_power(101, 153, 71) == 4);
 
-	// part 1 (note x and y are reversed)
-	// assert(solve(18, false) == "33,45");
-	// assert(solve(42, false) == "21,61");
+	// part 1
+	assert(solve(18, false) == "33,45");
+	assert(solve(42, false) == "21,61");
 
+	// part 2
 	assert(solve(18, true) == "90,269,16");
 	assert(solve(42, true) == "232,251,12");
 
