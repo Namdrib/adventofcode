04cee311ca39bf9cee39872b34013dbc904d57a3
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-12-12 17:25:08 +1030

    add 2018/day12 part 2

--- src/main/_2018/day12.cpp
+++ src/main/_2018/day12.cpp
@@ -7,23 +7,24 @@ using namespace std;
 string process(const string &input, map<string, char> rules) {
 
 	string next_gen;
-	for (int i = 2; i<input.size() - 1; i++) {
+	for (size_t i = 2; i<input.size() - 1; i++) {
 		string sub = input.substr(i-2, 5);
-		// cout << "s: " << string(i-1, ' ') << sub << endl;
-		next_gen += (rules[sub] == '#') ? "#" : ".";
+		char next = (rules.count(sub)) ? rules[sub] : '.';
+		next_gen += next;
 	}
 
 	return next_gen;
 }
 
-// No output value
-// Manually determine the correct output
-int solve(string input, map<string, char> rules, size_t num_generations, bool part_two) {
+size_t solve(string input, map<string, char> rules, size_t num_generations, bool part_two) {
 
+	size_t out, prev;
+	out = prev = 0;
 	set<string> seen;
 
-	int start = 0;
-	for (size_t i=0; i<num_generations; i++) {
+	int start = 0; // not sure if it's supposed to go negative, but it really does
+	size_t i=0;
+	for (i=0; i<num_generations; i++) {
 
 		// pad lhs with '.' if necessary
 		size_t first_plant = input.find_first_of("#");
@@ -32,51 +33,48 @@ int solve(string input, map<string, char> rules, size_t num_generations, bool pa
 			if (diff > 0) {
 				string temp(diff, '.');
 				input = temp + input;
-				start += temp.size() - 2;
-				// cout << "\tappend front: " << temp << endl;
+				start += diff - 2; // not sure why this is correct
 			}
 		}
 
 		// pad rhs with '.' if necessary
 		size_t last_plant = input.find_last_of("#");
 		if (last_plant != string::npos) {
-			// cout << "\tlast plant at " << last_plant << endl;
 			int diff = (last_plant + 5) - input.size();
-			// cout << "\tdiff : " << last_plant << " - " << input.size() << " = " << diff << endl;
 			if (diff > 0) {
 				string temp(diff, '.');
-				// cout << "\tappend back: " << temp << endl;
 				input += temp;
 			}
 		}
 
-		seen.insert(input);
+		// process and break early if state encountered previously
 		input = process(input, rules);
-
 		if (seen.count(input) > 0) {
-			cout << "seen " << input << " before, breaking" << endl;
 			break;
 		}
-		// cout << setw(2) << setfill(' ') << i+1 << ": " << input << endl;
-		// cout << "\tstart is at " << start << endl;
-	}
+		seen.insert(input);
 
-	int out = 0;
-	for (size_t i=0; i<input.size(); i++) {
-		if (input[i] == '#') {
-			out += i - start;
+		// sum of indices that contain plants
+		// offset by start
+		prev = out;
+		out = 0;
+		for (size_t i=0; i<input.size(); i++) {
+			if (input[i] == '#') {
+				out += i - start;
+			}
 		}
 	}
-	return out;
 
-	// return accumulate(all(input), 0, [](int a, char b) {
-	// 	return a + (b == '#');
-	// });
+	// there is a growth pattern
+	// use this to calculate the remaining sum
+	if (part_two) {
+		out += (50000000000 - i) * (out - prev);
+	}
+
+	return out;
 }
 
 int main() {
-	// int input;
-	// cin >> input;
 	vector<string> input = split_istream_per_line(cin);
 
 	map<string, char> rules;
@@ -88,14 +86,6 @@ int main() {
 		string result = temp.substr(temp.find_last_of(" ")+1);
 		rules[pattern] = result[0];
 	}
-
-	// part 1
-	// assert(solve(18, false) == "33,45");
-	// assert(solve(42, false) == "21,61");
-
-	// part 2
-	// assert(solve(18, true) == "90,269,16");
-	// assert(solve(42, true) == "232,251,12");
 
 	cout << "Part 1: " << solve(start, rules, 20, false) << endl;
 	cout << "Part 2: " << solve(start, rules, 50000000000, true) << endl;
