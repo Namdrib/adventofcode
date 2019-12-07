dc59a73bfd3b94ebec3abd2a57ce02426bff1112
Denton Phosavanh <dphosavanhau+github@gmail.com>
2019-12-07 23:18:08 +1030

    refactor 2019/day05

--- src/main/_2019/day05.cpp
+++ src/main/_2019/day05.cpp
@@ -4,96 +4,72 @@ using namespace std;
 
 // http://adventofcode.com/2019/day/5
 
-int solve(vector<int> mem, bool part_two = false)
+int solve(vector<int> program, bool part_two = false)
 {
 	const int puzzle_input = part_two ? 5 : 1;
 	int last_output = 0;
 
 	size_t ip = 0; // instruction pointer
 	bool keep_running = true;
-	while(keep_running && ip < mem.size())
+	while (keep_running && ip < program.size())
 	{
 		// assume input is valid - won't write out of bounds
-		int op_instruction = mem[ip];
+		int op_instruction = program[ip];
 		int opcode = op_instruction % 100;
 		bool is_param1_immediate = (op_instruction / 100) & 1;
 		bool is_param2_immediate = (op_instruction / 1000) & 1;
 		bool is_param3_immediate = (op_instruction / 10000) & 1;
 
-		int param1 = mem[ip + 1];
-		int param2 = mem[ip + 2];
-		int param3 = mem[ip + 3];
+		int param1 = program[ip + 1];
+		int param2 = program[ip + 2];
+		int param3 = program[ip + 3];
 
-		// early access of mem[paramX] results in segfault
-		int value1, value2;
+		int value1 = is_param1_immediate ? param1 : (param1 < (int) program.size()) ? program[param1] : 0;
+		int value2 = is_param2_immediate ? param2 : (param2 < (int) program.size()) ? program[param2] : 0;
 
-		switch(opcode)
+		switch (opcode)
 		{
 			case 1: // ADD
-				value1 = is_param1_immediate ? param1 : mem[param1];
-				value2 = is_param2_immediate ? param2 : mem[param2];
-				mem[param3] = value1 + value2;
-
+				program[param3] = value1 + value2;
 				ip += 4;
 				break;
 
 			case 2: // MULTIPLY
-				value1 = is_param1_immediate ? param1 : mem[param1];
-				value2 = is_param2_immediate ? param2 : mem[param2];
-				mem[param3] = value1 * value2;
-
+				program[param3] = value1 * value2;
 				ip += 4;
 				break;
 
 			case 3: // INPUT
-				mem[mem[ip + 1]] = puzzle_input;
-
+				program[param1] = puzzle_input;
 				ip += 2;
 				break;
 
 			case 4: // OUTPUT
-				last_output = mem[param1];
-				cout << "  write " << last_output << endl;
-
+				last_output = program[param1];
 				ip += 2;
 				break;
 
 			case 5: // JUMP-IF-TRUE
-			case 6: // JUMP-IF-FALSE
-				value1 = is_param1_immediate ? param1 : mem[param1];
-				value2 = is_param2_immediate ? param2 : mem[param2];
-				if ( (opcode == 5 ? (value1 != 0) :
-				   /* opcode == 6 */ value1 == 0) )
-				{
-					ip = value2;
-					break;
-				}
+				ip = (value1 != 0) ? value2 : ip + 3;
+				break;
 
-				ip += 3;
+			case 6: // JUMP-IF-FALSE
+				ip = (value1 == 0) ? value2 : ip + 3;
 				break;
 
 			case 7: // LESS THAN
 			case 8: // EQUALS
-			{
-				value1 = is_param1_immediate ? param1 : mem[param1];
-				value2 = is_param2_immediate ? param2 : mem[param2];
-				bool result = opcode == 7 ? (value1 < value2) : (value1 == value2);
-				mem[param3] = result;
-
+				program[param3] = (opcode == 7) ? (value1 < value2) : (value1 == value2);
 				ip += 4;
 				break;
-			}
 
 			case 99: // HALT
-				cout << "  stopped" << endl;
 				keep_running = false;
-
 				break;
 
 			default:
 				cout << "  something went wrong, opcode=" << opcode << endl;
 				keep_running = false;
-
 				break;
 		}
 	}
