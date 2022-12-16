9e6146ddc9e56c22f941d7614f7228e6c53af4f6
Namdrib <ph0fgazfl4j1@opayq.com>
2022-12-16 22:10:17 +1030

    refactor 2019/day10 (still WIP for part 2)
    - need to fix up the angle sweeping logic for part 2

--- src/main/_2019/day10.cpp
+++ src/main/_2019/day10.cpp
@@ -53,20 +53,20 @@ set<double> get_unique_angles_from(const vector<string> &v, size_t x, size_t y)
 
 			int dist_x = j - x;
 			int dist_y = i - y;
-			unique_angles.insert(atan2(dist_x, dist_y) * 180 / M_PI);
+			double angle = atan2(dist_x, -dist_y) * 180 / M_PI;
+			cout << "Angle from " << x << "," << y << " to " << j << "," << i << " = " << angle << endl;
+			unique_angles.insert(angle);
 		}
 	}
 
 	return unique_angles;
 }
 
-int solve(vector<string> v, bool part_two = false)
+size_t calculate_best_monitoring_location(vector<string> v, pair<size_t, size_t> &location)
 {
-	int out = 0;
-
-	int best_visible_asteroids = 0;
-	int best_y = 0;
-	int best_x = 0;
+	size_t best_visible_asteroids = 0;
+	size_t best_y = 0;
+	size_t best_x = 0;
 
 	// for each position with a asteroid
 	for (size_t i = 0; i < v.size(); i++)
@@ -78,7 +78,7 @@ int solve(vector<string> v, bool part_two = false)
 				continue;
 			}
 
-			int visible_asteroids = 0;
+			size_t visible_asteroids = 0;
 			set<double> unique_angles = get_unique_angles_from(v, j, i);
 			visible_asteroids = unique_angles.size();
 			// cout << j << ", " << i << " can see " << visible_asteroids << endl;
@@ -94,74 +94,94 @@ int solve(vector<string> v, bool part_two = false)
 	}
 
 	cout << "best is " << best_visible_asteroids << " at " << best_x << ", " << best_y << endl;
-	if (part_two)
-	{
-		// store the angle and distance of all asteroids relative to best one
-		// need to know the x and y to output the final answer
-		vector<blast> blasts;
-		for (size_t i = 0; i < v.size(); i++)
-		{
-			for (size_t j = 0; j < v[i].size(); j++)
-			{
-				if (i == best_y && j == best_x)
-				{
-					continue;
-				}
+	location.first = best_x;
+	location.second = best_y;
+	return best_visible_asteroids;
+}
 
-				blast temp = blast(best_x, best_y, j, i);
-				blasts.push_back(temp);
+size_t part_one(vector<string> v, bool part_two = false)
+{
+	pair<size_t, size_t> position;
+	size_t asteroids_detected = calculate_best_monitoring_location(v, position);
+	return asteroids_detected;
+}
+
+size_t part_two(vector<string> v)
+{
+	pair<size_t, size_t> position;
+	size_t asteroids_detected = calculate_best_monitoring_location(v, position);
+	size_t best_x = position.first;
+	size_t best_y = position.second;
+
+	// store the angle and distance of all asteroids relative to best one
+	// need to know the x and y to output the final answer
+	vector<blast> blasts;
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		for (size_t j = 0; j < v[i].size(); j++)
+		{
+			if ((i == best_y && j == best_x) || v[i][j] == '.')
+			{
+				continue;
+			}
+
+			blast temp = blast(best_x, best_y, j, i);
+			blasts.push_back(temp);
+		}
+	}
+	sort(all(blasts));
+
+	set<double> unique_angles = get_unique_angles_from(v, best_x, best_y);
+	cout << unique_angles << endl;
+
+	const size_t blast_limit = 200;
+	double current_angle = 0.0;
+	size_t index = 0;
+	size_t last_index = 0;
+	size_t final_x = 0;
+	size_t final_y = 0;
+
+  // TODO: Logic in here is still a bit broken
+	while (blasts[index].angle != current_angle)
+	{
+		index++;
+	}
+	for (size_t blast_number = 0; !blasts.empty(); blast_number++)
+	{
+		// skip all the ones of the same angle (since they're behind the current one)
+		while (blasts[index].angle == current_angle)
+		{
+			cout << "doin g stuff" << endl;
+			index = (index + 1) % blasts.size();
+
+			if (last_index == index)
+			{
+				index = 0;
+				break;
 			}
 		}
-		sort(all(blasts));
+		cout << "index=" << index << endl;
 
-		set<double> unique_angles = get_unique_angles_from(v, best_x, best_y);
-		cout << unique_angles << endl;
-
-		const int blast_limit = 200;
-		double current_angle = 90.0;
-		size_t index = 0;
-		size_t last_index = 0;
-		int final_x = 0;
-		int final_y = 0;
-		for (int blast_number = 0; !blasts.empty(); blast_number++)
+		// save coordinates of the blast_limit blast
+		if (blast_number == blast_limit)
 		{
-			// skip all the ones of the same angle (since they're behind the current one)
-			while (blasts[index].angle == current_angle)
-			{
-				index = (index + 1) % blasts.size();
-
-				if (last_index == index)
-				{
-					index = 0;
-					break;
-				}
-			}
-
-			// save coordinates of the blast_limit blast
-			if (blast_number == blast_limit)
-			{
-				final_x = blasts[index].x;
-				final_y = blasts[index].y;
-			}
-
-			// blast the asteroid away!
-			last_index = index;
-			current_angle = blasts[index].angle;
-			if (blast_number == 0)
-			{
-				cout << "first angle is " << current_angle << endl;
-			}
-			blasts.erase(blasts.begin() + index);
+			final_x = blasts[index].x;
+			final_y = blasts[index].y;
 		}
 
-		cout << blast_limit << "th one at " << final_x << ", " << final_y << endl;
-		out = final_x * 100 + final_y;
-	}
-	else
-	{
-		out = best_visible_asteroids;
+		// blast the asteroid away!
+		last_index = index;
+		current_angle = blasts[index].angle;
+		if (blast_number == 0)
+		{
+			cout << "rirst angle is " << current_angle << endl;
+		}
+		cout << "blasting asteroid " << blast_number << " at " << blasts[index].x << ", " << blasts[index].y << " with angle " << current_angle << endl;
+		blasts.erase(blasts.begin() + index);
 	}
 
+	cout << "Blasting " << blast_limit << "th asteroid at " << final_x << ", " << final_y << endl;
+	size_t out = final_x * 100 + final_y;
 	return out;
 }
 
@@ -176,6 +196,6 @@ int main()
 	cout << input.size() << endl;
 
 	// cout << atan2(0, 1) * 180 / M_PI << " " << atan2(1, 0) * 180 / M_PI << endl;
-	// cout << "Part 1: " << solve(input, false) << endl;
-	cout << "Part 2: " << solve(input, true) << endl; // incomplete, 2320 too high
+	cout << "Part 1: " << part_one(input) << endl;
+	cout << "Part 2: " << part_two(input) << endl; // incomplete, 2320 too high
 }
