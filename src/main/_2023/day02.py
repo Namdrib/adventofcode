6a692d6d87115f3c6f1e95a986cac4fa50632352
Namdrib <ph0fgazfl4j1@opayq.com>
2023-12-02 16:16:47 +1030

    Clean up 2023/day02

--- src/main/_2023/day02.py
+++ src/main/_2023/day02.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from collections import namedtuple
 from math import prod
 import sys
 
@@ -7,7 +6,8 @@ class Game:
 
     def __init__(self, id_: int):
         self.id: int = id_
-        # A list of {# Red, # Green, # Blue}
+
+        # A list of [# Red, # Green, # Blue]
         self._samples: list = []
 
         self._colour_to_index: dict = {
@@ -18,24 +18,24 @@ class Game:
 
     def add_sample(self, sample_str: str) -> None:
         """
-        Record a sample into our samples
+        Record a sample into our samples.
+        Regardless of the order of the given sample, always save is as [R, G, B]
 
         :param sample_str: A string like "3 blue, 4 red"
         :type sample_str: str
         """
-        # print(f'Adding {sample_str=}')
         sample_result = [0, 0, 0]
         for sample_components in sample_str.split(', '):
             number, colour = sample_components.split(' ')
             sample_result[self._colour_to_index[colour]] = int(number)
             self._samples.append(sample_result)
 
-    def potential_max_cubes(self):
+    def minimum_possible_cubes(self) -> list:
         """
         Returns the potential maximum number of each colour of cubes in the bag
         Calculated based on the samples
-        :return: A tuple of (R, G, B) of the potential maximum cubes the bag has
-        :rtype: Tuple
+        :return: A list of [R, G, B] of the potential maximum cubes the bag has
+        :rtype: list
         """
         out = [0, 0, 0]
         for sample in self._samples:
@@ -43,22 +43,18 @@ class Game:
             out[1] = max(out[1], sample[1])
             out[2] = max(out[2], sample[2])
 
-        print(f'{self.id} max: {out}')
         return out
 
     def can_be_played_with(self, contents: list) -> bool:
         """
-        Return whether this game can be played with the given contents
+        Return whether this game can be played with the given contents of [R, G, B]
         Calculated based on the samples
 
         :return: True if we can play a game with the given contents
-        :rtype: Tuple
+        :rtype: bool
         """
-        max_cubes: list = self.potential_max_cubes()
+        max_cubes: list = self.minimum_possible_cubes()
         return all(max_cubes[i] <= contents[i] for i in range(len(contents)))
-
-    def calculate_power(self) -> int:
-        return prod(self.potential_max_cubes())
 
 class Day02:
     """
@@ -77,7 +73,7 @@ class Day02:
     def read_input(self) -> None:
         """
         Read input from stdin and parse it into a useful data structure
-        In this case, each line contains an alphanumeric string
+        In this case, each line contains a game with a number of samples with numbers of coloured cubes
         """
         _input = sys.stdin.read()
 
@@ -87,43 +83,37 @@ class Day02:
                 game_id_str, sample_str = line.split(': ')
                 game_id: int = int(game_id_str[game_id_str.find(' ')+1:])
                 g = Game(game_id)
-                print(f'Creating game {game_id}')
 
+                # Build up the list of samples the game has seen
                 samples: list = sample_str.split('; ')
                 for sample in samples:
                     g.add_sample(sample)
 
-                print(g._samples)
-
                 self._games.append(g)
 
-
-    def get_calibration_value(self, line: str) -> int:
+    def calculate_power(self, cubes: list) -> int:
         """
-        Combine the first digit and the last digit to form a single two-digit number
+        The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together.
 
-        :param line: A line from the calibration document
-        :type line: str
-        :return: The concatenated two-digit number
+        :param cubes: A [R, G, B] list of cubes
+        :type cubes: list
+        :return: The product of the number of cubes
         :rtype: int
         """
-        # Keep it as a string so it can be concatenated
-        digits: list = [x for x in line if x.isdigit()]
-        calibration_value: str = digits[0] + digits[-1]
-        return int(calibration_value)
+        return prod(cubes)
 
     def part_one(self) -> int:
         """
-        Return the sum of all of the calibration values
+        Return the sum of all of the game IDs that can be played with the available cubes
         """
-        return sum(x.id for x in self._games if x.can_be_played_with([12, 13, 14]))
+        available_cubes: list = [12, 13, 14]
+        return sum(x.id for x in self._games if x.can_be_played_with(available_cubes))
 
     def part_two(self) -> int:
         """
-        Return the sum of all of the calibration values after pre-processing the document
+        Return the sum of the powers of the minimal possible set of cubes in a game
         """
-        # Represents the calibration doc after translating the number words to digits
-        return sum(x.calculate_power() for x in self._games)
+        return sum(self.calculate_power(x.minimum_possible_cubes()) for x in self._games)
 
 def main() -> None:
     """
