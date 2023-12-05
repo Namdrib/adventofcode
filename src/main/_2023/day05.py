2b6f9755e1928c564f432c47cb3238bbc6a4bc48
Namdrib <ph0fgazfl4j1@opayq.com>
2023-12-05 23:40:34 +1030

    2023/day05 part 2 still incomplete

--- src/main/_2023/day05.py
+++ src/main/_2023/day05.py
@@ -7,8 +7,6 @@ class Mapping:
         self.source = source
         self.length = length
 
-        self.max = self.source + self.length
-
     def translate(self, n: int) -> int:
         """
         Translate a number by checking if it is in the range described by [source, source+length]
@@ -20,9 +18,17 @@ class Mapping:
         :return: The number after translation
         :rtype: int
         """
-        if self.source <= n <= self.max:
+        if self.source <= n <= self.source + self.length:
             # print(f'  {n=} matches [{self.source=}, {self.max=}], resulting in {self.dest + (n - self.source)}')
             return self.dest + (n - self.source)
+        return n
+    
+    def translate_reverse(self, n: int) -> int:
+        """
+        Translate a number in reverse by treating it as a destination, and seeing the [source, source+length] could have produced it
+        """
+        if self.dest <= n <= self.dest + self.length:
+            return self.source + (n - self.dest)
         return n
 
     def __str__(self) -> str:
@@ -95,24 +101,27 @@ class Day05:
                 elif map_name == 'humidity-to-location':
                     self.humidity_to_location_map.append(mapping)
 
-    def translate(self, n: int, map_: list) -> int:
+    def translate(self, n: int, map_: list, reverse: bool=False) -> int:
         """
-        _summary_
+        Translate a number `n` according to the map `map_`, which contains a list of mappings
 
         :param n: The number to transform
         :type n: int
         :param map_: A list of all of the potential transformations
         :type map_: list
+        :param reverse: Whether to do a reverse-translation
+        :type reverse: bool
         :return: The result of translating `n`, if applicable. If not, then return n
         :rtype: int
         """
         old_n = n
         for mapping in map_:
-            # print(f'Does {n=} fit in {mapping=}?')
 
-            n = mapping.translate(n)
+            if reverse:
+                n = mapping.translate_reverse(n)
+            else:
+                n = mapping.translate(n)
             if n != old_n:
-                # print(f'Yes: {n=}')
                 return n
 
         return n
@@ -126,7 +135,7 @@ class Day05:
         :return: The final location of the seed after going through all the maps
         :rtype: int
         """
-        print(f'Translating seed number {seed_num}')
+        # print(f'Translating {seed_num=}')
         soil: int       = self.translate(seed_num, self.seed_to_soil_map)
         fertiliser: int = self.translate(soil, self.soil_to_fertiliser_map)
         water: int      = self.translate(fertiliser, self.fertiliser_to_water_map)
@@ -134,10 +143,36 @@ class Day05:
         temp: int       = self.translate(light, self.light_to_temp_map)
         humidity: int   = self.translate(temp, self.temp_to_humidity_map)
         location: int   = self.translate(humidity, self.humidity_to_location_map)
-        print(f'  {seed_num} -> {soil} -> {fertiliser} -> {water} -> {light} -> {temp} -> {humidity} -> {location}')
+        # print(f'  {seed_num=} -> {soil} -> {fertiliser} -> {water} -> {light} -> {temp} -> {humidity} -> {location=}')
 
         return location
 
+    def get_seed_from_location(self, location: int) -> int:
+        # print(f'Reverse translating {location=}')
+        humidity: int   = self.translate(location, self.humidity_to_location_map, reverse=True)
+        temp: int       = self.translate(humidity, self.temp_to_humidity_map, reverse=True)
+        light: int      = self.translate(temp, self.light_to_temp_map, reverse=True)
+        water: int      = self.translate(light, self.water_to_light_map, reverse=True)
+        fertiliser: int = self.translate(water, self.fertiliser_to_water_map, reverse=True)
+        soil: int       = self.translate(fertiliser, self.soil_to_fertiliser_map, reverse=True)
+        seed_num: int   = self.translate(soil, self.seed_to_soil_map, reverse=True)
+        # print(f'  {location=} -> {humidity} -> {temp} -> {light} -> {water} -> {fertiliser} -> {soil} -> {seed_num=}')
+
+        return seed_num
+    
+    def is_valid_seed(self, seed_num: int) -> bool:
+        """
+        Return whether a given seed number is included in the known seed range
+
+        :param seed_num: The seed number to check
+        :type seed_num: int
+        :return: True if the given seed number is valid, False otherwise
+        :rtype: bool
+        """
+        for i in range(0, len(self.seeds), 2):
+            if seed_num in range(self.seeds[i], self.seeds[i] + self.seeds[i+1]):
+                return True
+        return False
 
     def part_one(self) -> int:
         """
@@ -147,18 +182,38 @@ class Day05:
 
     def part_two(self) -> int:
         """
-        Return the number of scratchcards you end up with with the new scoring mechanism
-        You win copies of the scratchcards below the winning card equal to the number of matches.
-        So, if card 10 were to have 5 matching numbers, you would win one copy each of cards 11, 12, 13, 14, and 15.
+        Return the lowest location number that corresponds to any of the initial seed numbers
         """
-        lowest_location_number: int = 999999
-        for i in range(0, len(self.seeds), 2):
-            # print(f'Seed {self.seeds[i]} has range {self.seeds[i+1]}')
-            for seed_num in range(self.seeds[i], self.seeds[i]+self.seeds[i+1]):
-                location_number = self.get_location_from_seed(seed_num)
-                lowest_location_number = min(lowest_location_number, location_number)
+        # # here's a list of all the locations that can be mapped to
+        # possible_seeds: list = []
+        # possible_locations: list = []
+        # for location_mapping in sorted(self.humidity_to_location_map, key=lambda x: x.dest + x.length):
+        #     lower_location = location_mapping.dest
+        #     upper_location = location_mapping.dest + location_mapping.length
 
-        return lowest_location_number
+        #     for test_location in range(lower_location, upper_location):
+        #         seed_num: int = self.get_seed_from_location(test_location)
+        #         if self.is_valid_seed(seed_num):
+        #             possible_seeds.append(seed_num)
+        #             possible_locations.append(test_location)
+        # return min(possible_locations)
+
+        # Find the lowest location that is a valid sed
+        test_location: int = 0
+        while True:
+            seed_num: int = self.get_seed_from_location(test_location)
+            if self.is_valid_seed(seed_num):
+                return test_location
+            test_location += 1
+
+        # lowest_location_number: int = 999999
+        # for i in range(0, len(self.seeds), 2):
+        #     # print(f'Seed {self.seeds[i]} has range {self.seeds[i+1]}')
+        #     for seed_num in range(self.seeds[i], self.seeds[i]+self.seeds[i+1]):
+        #         location_number = self.get_location_from_seed(seed_num)
+        #         lowest_location_number = min(lowest_location_number, location_number)
+
+        # return lowest_location_number
 
 def main() -> None:
     """
@@ -169,6 +224,7 @@ def main() -> None:
 
     print(f'Part 1: {solver.part_one()}')
     print(f'Part 2: {solver.part_two()}')
+    # 79874952 too high
 
 if __name__ == '__main__':
     main()
