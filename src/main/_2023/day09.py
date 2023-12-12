7e611a3913c4f8ef5715f3fe3cd4d8a751257229
Namdrib <ph0fgazfl4j1@opayq.com>
2023-12-12 20:02:24 +1030

    Add 2023/day09 part 2 + docstrings

--- src/main/_2023/day09.py
+++ src/main/_2023/day09.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import itertools
-import math
 import sys
 
 class Day09:
@@ -26,37 +25,75 @@ class Day09:
         for line in self.input.splitlines(keepends=False):
             self.sensor_readings.append(list(map(int, line.split())))
 
-        for sensor_reading in self.sensor_readings:
-            print(sensor_reading)
-
     def next_sequence(self, values: list) -> list:
-        out: list = []
-        for i, value in enumerate(values):
-            if i < len(values)-1:
-                out.append(values[i+1] - value)
+        """
+        The next sequence is a list whose values are the differences of each
+        value and its previous neighbour
 
-        return out
+        :param values: The values to generate a sequence from
+        :type values: list
+        :return: The next sequence of the values
+        :rtype: list
+        """
+        return [b - a for a, b in itertools.pairwise(values)]
 
     def get_sequences(self, history: list) -> int:
+        """
+        Generate sequences from a reading until the last sequence is all zeros
+
+        :param history: The history to generate sequences from
+        :type history: list
+        :return: All sequences generated from the history, until the last is all zeros
+        :rtype: int
+        """
+        # So we don't modify history
         sequences: list = [list(history)]
+
+        # Keep generating sequences till we get one that's all zeros
         while not all(x == 0 for x in sequences[-1]):
-            next_row = self.next_sequence(sequences[-1])
-            sequences.append(next_row)
-        
+            sequences.append(self.next_sequence(sequences[-1]))
+
         return sequences
 
     def get_extrapolated_value(self, reading: list) -> int:
+        """
+        Extrapolate by adding 0 to the end of the sequence of 0s,
+        then fill in the last values for each previous sequence
+
+        :param reading: The sensor reading to extrapolate
+        :type reading: list
+        :return: The extrapolated value
+        :rtype: int
+        """
         sequences: list = self.get_sequences(reading)
         sequences[-1].append(0)
 
+        # Starting from the last sequence, populate the last element of each sequence
         for i in range(len(sequences)-1, 0, -1):
-            next_value: int = sequences[i][-1] + sequences[i-1][-1]
+            next_value: int = sequences[i-1][-1] + sequences[i][-1]
             sequences[i-1].append(next_value)
 
-        print(f'New sequences:')
-        for sequence in sequences:
-            print(f'  {sequence}')
         return sequences[0][-1]
+
+    def get_back_extrapolated_value(self, reading: list) -> int:
+        """
+        Extrapolate backwards by adding 0 to the beginning of the sequence of 0s,
+        then fill in the first values for each previous sequence
+
+        :param reading: The sensor reading to extrapolate backwards from
+        :type reading: list
+        :return: The extrapolated value
+        :rtype: int
+        """
+        sequences: list = self.get_sequences(reading)
+        sequences[-1].insert(0, 0)
+
+        # Starting from the last sequence, populate the first element of each sequence
+        for i in range(len(sequences)-1, 0, -1):
+            next_value: int = sequences[i-1][0] - sequences[i][0]
+            sequences[i-1].insert(0, next_value)
+
+        return sequences[0][0]
 
     def part_one(self) -> int:
         """
@@ -66,10 +103,9 @@ class Day09:
 
     def part_two(self) -> int:
         """
-        Find how many steps it takes before all of the starting nodes are simultaneously on nodes that end with Z
+        Find the sum of the extrapolated values of each sensor reading when extrapolating backwards
         """
-        ARRAY: list = []
-        return sum(self.calc(x) for x in ARRAY)
+        return sum(self.get_back_extrapolated_value(x) for x in self.sensor_readings)
 
 def main() -> None:
     """
