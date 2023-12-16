ab385d247895eba711b52df9966972226d020268
Namdrib <ph0fgazfl4j1@opayq.com>
2023-12-17 01:36:32 +1030

    Add 2023/day16 part 2

--- src/main/_2023/day16.py
+++ src/main/_2023/day16.py
@@ -4,6 +4,8 @@ from queue import Queue
 import sys
 
 class Direction(Enum):
+    # The Directions can be mapped to their values to get
+    # a useful index for determining change in position, and arrow type
     UP = 0
     DOWN = 1
     LEFT = 2
@@ -11,9 +13,9 @@ class Direction(Enum):
 
 class Beam:
     def __init__(self, x: int, y: int, direction: Direction):
-        self.x = x
-        self.y = y
-        self.direction = direction
+        self.x: int = x
+        self.y: int = y
+        self.direction: Direction = direction
 
 class Day16:
     """
@@ -29,10 +31,6 @@ class Day16:
         # A 2D grid of spaces and mirrors
         self.grid: list = []
 
-        # Keep track of which tiles in the grid are energised
-        # Each element is a list of Directions
-        self.beams: list = []
-
         # The indices of dx and dy line up with Direction
         self.dx: list = [0, 0, -1, 1]
         self.dy: list = [-1, 1, 0, 0]
@@ -45,15 +43,11 @@ class Day16:
         """
         self.input = sys.stdin.read()
 
-        self.grid = [x for x in self.input.splitlines(keepends=False)]
-        for line in self.grid:
-            self.beams.append([[] for x in line])
-
-        self.visualise_grid()
+        self.grid = list(self.input.splitlines(keepends=False))
 
     def calculate_next_directions(self, direction: Direction, tile: str) -> list:
         """
-        The behavour of a beam will depend on what it encounters as it moves:
+        The behaviour of a beam will depend on what it encounters as it moves:
         - If the beam encounters empty space (.), it continues in the same direction
         - If the beam encounters a mirror (/ or \\), the beam is reflected 90 degrees depending on the angle fo the mirror
         - If the beam encounters the pointy end of a splitter (| or -), the beam passes through the splitter as if the splitter were empty space
@@ -63,7 +57,7 @@ class Day16:
         :type direction: Direction
         :param tile: The space it is encountering
         :type tile: str
-        :return: The new directions it will move in next (0 or more)
+        :return: The next directions it will move in next (0 or more)
         :rtype: list
         """
         # Continue in the same direction
@@ -110,7 +104,7 @@ class Day16:
         print(f'Unexpected direction, {direction=}, {tile=}')
         return direction
 
-    def visualise_grid(self):
+    def visualise_grid(self, beams: list):
         """
         Beams are only shown on empty tiles
         Arrows indicate the direction of the beams
@@ -120,10 +114,10 @@ class Day16:
             for col, char in enumerate(line):
                 if char in '|-\\/':
                     print(char, end='')
-                elif len(self.beams[row][col]) > 1:
-                    print(len(self.beams[row][col]), end='')
-                elif len(self.beams[row][col]) == 1:
-                    direction_index: int = self.beams[row][col][0].value
+                elif len(beams[row][col]) > 1:
+                    print(len(beams[row][col]), end='')
+                elif len(beams[row][col]) == 1:
+                    direction_index: int = beams[row][col][0].value
                     arrow: str = self.arrows[direction_index]
                     print(arrow, end='')
                 else:
@@ -131,18 +125,23 @@ class Day16:
             print()
         print()
 
-    def propagate(self, starting_beam: Beam):
+    def propagate(self, starting_beam: Beam) -> list:
         """
-        Propagate a beam heading in a direction
+        Bounce a beam around the grid, reflecting off mirrors and splitters
 
-        :param beam: The beam to propagate
-        :type beam: Beam
-        :return: The new beams created by this beam, if any
+        :param starting_beam: The beam to propagate
+        :type starting_beam: Beam
+        :return: All of the beams that will be produced from this starting beam
         :rtype: list
         """
+        # Keep track of which tiles in the grid are energised
+        # Each element is a list of Directions
+        beams: list = []
+        for line in self.grid:
+            beams.append([[] for x in line])
 
+        # Using a queue (instead of a recursive stack) prevents the stack from overflowing
         beam_queue: Queue = Queue()
-
         beam_queue.put(starting_beam)
 
         while not beam_queue.empty():
@@ -153,26 +152,36 @@ class Day16:
                 continue
 
             # There is a loop. No point continuing down this path
-            if beam.direction in self.beams[beam.y][beam.x]:
+            if beam.direction in beams[beam.y][beam.x]:
                 continue
 
-            # Record the beam
-            self.beams[beam.y][beam.x].append(beam.direction)
-
             # Energise this tile
-            print(f'Lighting up tile ({beam.x}, {beam.y})')
-            # self.visualise_grid()
+            beams[beam.y][beam.x].append(beam.direction)
 
-            # See what to do with the current beam
+            # See how this beam behaves (where is it going next?)
             current_tile: str = self.grid[beam.y][beam.x]
             next_directions: list = self.calculate_next_directions(beam.direction, current_tile)
 
+            # Create a new beam for each of those directions
             for next_direction in next_directions:
                 direction_index: int = next_direction.value
-                new_x: int = beam.x + self.dx[direction_index]
-                new_y: int = beam.y + self.dy[direction_index]
-                new_beam: Beam = Beam(new_x, new_y, next_direction)
-                beam_queue.put(new_beam)
+                next_x: int = beam.x + self.dx[direction_index]
+                next_y: int = beam.y + self.dy[direction_index]
+                next_beam: Beam = Beam(next_x, next_y, next_direction)
+                beam_queue.put(next_beam)
+
+        return beams
+
+    def count_energised_tiles(self, beams: list) -> int:
+        """
+        Return the number of energised tiles, given a beam pattern
+
+        :param beams: Where the beams are pointing, and in which tiles
+        :type beams: list
+        :return: The number of energised tiles
+        :rtype: int
+        """
+        return sum(sum(1 for tile in row if tile) for row in beams)
 
     def part_one(self) -> int:
         """
@@ -180,15 +189,55 @@ class Day16:
         """
         beam_start: Beam = Beam(0, 0, Direction.RIGHT)
 
-        self.propagate(beam_start)
+        beams: list = self.propagate(beam_start)
 
-        self.visualise_grid()
-        return sum(sum(1 for tile in row if tile) for row in self.beams)
+        self.visualise_grid(beams)
+
+        return self.count_energised_tiles(beams)
+
 
     def part_two(self) -> int:
         """
+        Beams may entry from any edge tile, heading away from that edge.
+        Find the starting position that energises as many tiles as possible
+        Note: takes a while to run
         """
-        return 0
+        max_energised_tiles: int = 0
+        for i in range(len(self.grid)):
+            # Check all starting positions from the left edge
+            beam_start: Beam = Beam(0, i, Direction.RIGHT)
+            beams: list = self.propagate(beam_start)
+
+            # Count the number of energised tiles, see if it is the max
+            num_energised_tiles: int = self.count_energised_tiles(beams)
+            max_energised_tiles = max(max_energised_tiles, num_energised_tiles)
+
+            # Check all starting positions from the right edge
+            beam_start: Beam = Beam(len(self.grid[i])-1, i, Direction.LEFT)
+            beams: list = self.propagate(beam_start)
+
+            # Count the number of energised tiles, see if it is the max
+            num_energised_tiles: int = self.count_energised_tiles(beams)
+            max_energised_tiles = max(max_energised_tiles, num_energised_tiles)
+
+        for j in range(len(self.grid[0])):
+            # Check all starting positions from the top edge
+            beam_start: Beam = Beam(j, 0, Direction.DOWN)
+            beams: list = self.propagate(beam_start)
+
+            # Count the number of energised tiles, see if it is the max
+            num_energised_tiles: int = self.count_energised_tiles(beams)
+            max_energised_tiles = max(max_energised_tiles, num_energised_tiles)
+
+            # Check all starting positions from the bottom edge
+            beam_start: Beam = Beam(j, len(self.grid)-1, Direction.UP)
+            beams: list = self.propagate(beam_start)
+
+            # Count the number of energised tiles, see if it is the max
+            num_energised_tiles: int = self.count_energised_tiles(beams)
+            max_energised_tiles = max(max_energised_tiles, num_energised_tiles)
+
+        return max_energised_tiles
 
 def main() -> None:
     """
