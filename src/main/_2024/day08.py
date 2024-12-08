d704b3ffce7d0b211cc60aba1a9a0de424382607
Namdrib <ph0fgazfl4j1@opayq.com>
2024-12-08 16:40:55 +1030

    2024/day08: Slight refactoring
    - Use itertools.product() to save a level of indenting
    - Add some comments
    - Rename some vars

--- src/main/_2024/day08.py
+++ src/main/_2024/day08.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import itertools
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -39,57 +40,68 @@ class Day08:
         # The count of antinodes. Starts off all False
         # As we encounter antinodes, they are marked as True
         # We only care about unique locations, so bool is good enough
-        antinode_locations: list = [[False for x in y] for y in self.grid]
+        antinodes: list = [[False for x in y] for y in self.grid]
 
         # Loop over the locations of each antenna type separately
         for locations in self.antenna_locations.values():
-            # Loop over each pair of antennas of the same type
-            for L1 in locations:
-                for L2 in locations:
-                    # For part 2, all antennas are implicitly antinodes
-                    if not part_two and L1 == L2:
-                        continue
+            # Nested loop over each pair of antennas of the same type
+            for L1, L2 in itertools.product(locations, repeat=2):
+                # Allow "same pair" antennas for part 2, otherwise skip
+                # Add the location here to avoid looping `limit` times below
+                if L1 is L2:
+                    if part_two:
+                        antinodes[L1[1]][L1[0]] = True
+                    continue
 
-                    # How many times to project forwards?
-                    # For part one, only look one step ahead
-                    # For part two, keep going until out of bounds
-                    loops: int = 0
-                    limit = 9999 if part_two else 1
+                # How many times to project forwards?
+                # For part one, only look one step ahead
+                # For part two, keep going until out of bounds
+                loops: int = 0
+                limit = 9999 if part_two else 1
 
-                    new_x, new_y = L1[0], L1[1]
-                    while loops < limit:
-                        # Project one space forward by the difference
-                        new_x += (L1[0] - L2[0])
-                        new_y += (L1[1] - L2[1])
+                # This _could_ be done mathematically for each antenna pair by
+                # counting how many multiples of dx and dy can be added to L1
+                # until one of them goes out of bounds.
+                # But we'd still need to keep track of _unique_ antenna spots.
+                # Simply applying an equation doesn't take that into account.
+                # e.g., min( ((len(grid) - L1[1]) / dy), (len(grid[0]) - L1[0]) / dx) )
+                # There may be some edge cases / rounding to consider
+                # However, this way, we can keep the same code for both parts
+                # without needing to do too much thinking :)
+                new_x, new_y = L1[0], L1[1]
+                while loops < limit:
+                    # Project one space forward by the difference
+                    new_x += (L1[0] - L2[0])
+                    new_y += (L1[1] - L2[1])
 
-                        # If this step is in the grid, mark it as an antinode
-                        if helpers.in_range(self.grid, new_y) and helpers.in_range(self.grid[0], new_x):
-                            antinode_locations[new_y][new_x] = True
-                        # We've gone off-grid, this is the last antinode for
-                        # this pair of antennas
-                        else:
-                            break
+                    # Is the new location in the grid?
+                    if helpers.in_range(self.grid, new_y) and helpers.in_range(self.grid[0], new_x):
+                        # Mark it as an antinode
+                        antinodes[new_y][new_x] = True
+                    else:
+                        # We've gone off-grid, check the next pair of antennas
+                        break
 
-                        loops += 1
+                    loops += 1
 
-        return antinode_locations
+        return antinodes
 
     def part_one(self) -> int:
         """
         Return the number of unique locations that are antinodes when allowing
         one step
         """
-        counts: list = self.calculate_antinode_locations(part_two=False)
+        antinode_locations: list = self.calculate_antinode_locations(part_two=False)
         # Count the number of locations in counts that have a non-zero number
-        return sum(sum(row) for row in counts)
+        return sum(sum(row) for row in antinode_locations)
 
     def part_two(self) -> int:
         """
         Return the number of unique locations that are antinodes when allowing
         many steps
         """
-        counts: list = self.calculate_antinode_locations(part_two=True)
-        return sum(sum(row) for row in counts)
+        antinodes: list = self.calculate_antinode_locations(part_two=True)
+        return sum(sum(row) for row in antinodes)
 
 def main() -> None:
     """
