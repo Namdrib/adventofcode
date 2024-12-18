877d200f35a2f7209affaa9a48a423ebaab08829
Namdrib <ph0fgazfl4j1@opayq.com>
2024-12-18 20:25:36 +1030

    2024/day06: Performance boost part 2 (again)
    Using tuples for the (x, y) position is faster than using helpers.Point
    - 15-20 seconds down to about 7
    
    Tried using namedtuples, but it seemed to be the same as helpers.Point

--- src/main/_2024/day06.py
+++ src/main/_2024/day06.py
@@ -41,40 +41,45 @@ class Day06:
     def traverse_grid(self, grid: list) -> set:
         # Start at the start, facing up
         next_dir = helpers.Point(0, -1)
-        p = helpers.Point(self.start_point.x, self.start_point.y)
+        # Using a tuple instead of helpers.Point for a performance boost (around
+        # 15-20 seconds down to ~7 seconds for part 2)
+        guard_pos = (self.start_point.x, self.start_point.y)
 
         # Used to determine whether we've already been here so we don't
         # double-count the same location
-        seen = set()
+        visited = set()
         # Used to determine whether we're stuck in a loop
-        seen_with_direction = set()
+        visited_with_direction = set()
 
         # Walk forwards until we loop or exit the grid
         while True:
-            if p in seen:
-                p_with_direction = (p.x, p.y, next_dir.x, next_dir.y)
+            if guard_pos in visited:
+                guard_pos_with_direction = (
+                    guard_pos[0], guard_pos[1], next_dir.x, next_dir.y
+                )
                 # Been here in the same direction, it's a loop
-                if p_with_direction in seen_with_direction:
+                if guard_pos_with_direction in visited_with_direction:
                     return set()
 
                 # But allow for crossing over a point we've been to before
-                seen_with_direction.add(p_with_direction)
+                visited_with_direction.add(guard_pos_with_direction)
 
-            seen.add(p.clone())
+            visited.add(guard_pos)
 
-            next_point = helpers.Point(p.x + next_dir.x, p.y + next_dir.y)
+            next_pos = (guard_pos[0] + next_dir.x, guard_pos[1] + next_dir.y)
 
             # Out of bounds, the guard made it out of the grid.
             # Return the set of points they've been to
-            if not helpers.in_range_2d(grid, next_point.x, next_point.y):
-                return seen
+            if not helpers.in_range_2d(grid, next_pos[0], next_pos[1]):
+                return visited
 
             # Space in front, walk forwards
-            if grid[next_point.y][next_point.x] == '.':
-                p.add(next_dir)
+            next_space: str = grid[next_pos[1]][next_pos[0]]
+            if next_space == '.':
+                guard_pos = (guard_pos[0] + next_dir.x, guard_pos[1] + next_dir.y)
 
             # Obstacle in front, rotate 90 degrees clockwise
-            elif grid[next_point.y][next_point.x] == '#':
+            elif next_space == '#':
                 next_dir = helpers.rotate_clockwise(next_dir)
 
             else:
@@ -95,17 +100,17 @@ class Day06:
 
         points_visited: set = self.traverse_grid(self.grid)
 
-        # All the possible places we can put an obstacle that may affect the
-        # guard's path
+        # All the places where an obstacle may affect the guard's path
         for p in points_visited:
-            self.grid[p.y][p.x] = '#'
+            # Place an obstacle
+            self.grid[p[1]][p[0]] = '#'
 
             # Did this cause a loop?
             if len(self.traverse_grid(self.grid)) == 0:
                 count += 1
 
             # Put it back the way it was, ready for the next iteration
-            self.grid[p.y][p.x] = '.'
+            self.grid[p[1]][p[0]] = '.'
 
         return count
 
