be77a0884025fc89350da9adcfdf9f4a8a0923ce
Namdrib <ph0fgazfl4j1@opayq.com>
2024-12-23 16:07:40 +1030

    Add 2024/day23 part 1

--- src/main/_2024/day23.py
+++ src/main/_2024/day23.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import itertools
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -16,6 +17,9 @@ class Day23:
         """
         self.input: list = None
 
+        self.connections: set = None
+        self.all_connections: set = None
+
     def read_input(self) -> None:
         """
         Read input from stdin and parse it into a useful data structure
@@ -24,15 +28,47 @@ class Day23:
         raw_input = sys.stdin.read()
         self.input = raw_input.splitlines()
 
+        self.connections = set()
         for item in self.input:
-            pass
+            connection: tuple = tuple(item.split('-'))
+            self.connections.add(connection)
+
+    def compute_computer_sets(self) -> None:
+        # {computer_name: [computer_names]}
+        self.all_connections: dict = {}
+        for c in self.connections:
+            if c[0] not in self.all_connections:
+                self.all_connections[c[0]] = []
+            if c[1] not in self.all_connections:
+                self.all_connections[c[1]] = []
+
+            self.all_connections[c[0]].append(c[1])
+            self.all_connections[c[1]].append(c[0])
+
+    def get_sets_of_three(self) -> set:
+        tri_connections: set = set()
+
+        # We already know a and b are connected
+        for a, b in self.connections:
+            # See if there are any computers that are connected to both A and B
+            for computer, connections in self.all_connections.items():
+                if a in connections and b in connections:
+                    tri_connections.add(tuple(sorted((a, b, computer))))
+
+        return tri_connections
 
     def part_one(self) -> int:
         """
         Return the ...
         """
-        count: int = 0
+        self.compute_computer_sets()
+        tri_connections: set = self.get_sets_of_three()
 
+        # Count the tri-connections where any of the computers starts with t
+        count: int = 0
+        for tc in tri_connections:
+            if any(x.startswith('t') for x in tc):
+                count += 1
         return count
 
     def part_two(self) -> int:
