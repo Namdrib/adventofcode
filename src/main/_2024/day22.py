47f78c075817aa64e6771c57a5827458246a0931
Namdrib <ph0fgazfl4j1@opayq.com>
2024-12-23 17:17:22 +1030

    Refactor 2024/day22
    Some slight performance boost (~7s -> ~6s)

--- src/main/_2024/day22.py
+++ src/main/_2024/day22.py
@@ -25,38 +25,6 @@ class Day22:
 
         self.initial_numbers: list = list(map(int, self.input))
 
-    def mix(self, num: int, mixer: int) -> int:
-        """
-        To mix a value into the secret number, calculate the bitwise XOR of the
-        given value and the secret number.
-        Then, the secret number becomes the result of that operation.
-        (If the secret number is 42 and you were to mix 15 into the secret
-        number, the secret number would become 37.)
-
-        :param num: The secret number
-        :type num: int
-        :param mixer: The number to mix into the secret number
-        :type mixer: int
-        :return: The value of the secret number after mixing
-        :rtype: int
-        """
-        return num ^ mixer
-
-    def prune(self, num: int) -> int:
-        """
-        To prune the secret number, calculate the value of the secret number
-        modulo 16777216.
-        Then, the secret number becomes the result of that operation.
-        (If the secret number is 100000000 and you were to prune the secret
-        number, the secret number would become 16113920.)
-
-        :param num: The number to prune
-        :type num: int
-        :return: The number after pruning
-        :rtype: int
-        """
-        return num % 16777216
-
     def next_secret_number(self, num: int) -> int:
         """
         Perform steps to generate the next iteration of a secret number
@@ -73,26 +41,37 @@ class Day22:
         Then, mix this result into the secret number.
         Finally, prune the secret number.
 
+        To mix a value into the secret number, calculate the bitwise XOR of the
+        given value and the secret number.
+        Then, the secret number becomes the result of that operation.
+        If the secret number is 42 and you were to mix 15 into the secret
+        number, the secret number would become 37.
+
+        To prune the secret number, calculate the value of the secret number
+        modulo 16777216.
+        Then, the secret number becomes the result of that operation.
+        If the secret number is 100000000 and you were to prune the secret
+        number, the secret number would become 16113920.
+
+        Mixing and pruning are done in-line, rather than as helper functions to
+        help reduce runtime (fewer calls on and off the call stack).
 
         :param num: The secret number to do stuff to
         :type num: int
         :return: The next iteration of the secret number, according to the steps
         :rtype: int
         """
-        # Step 1
-        mult: int = num * 64
-        num = self.mix(num, mult)
-        num = self.prune(num)
+        # Step 1: Multiply by 64, mix, and prune
+        num ^= (num << 6)
+        num %= 16777216
 
-        # Step 2
-        div: int = int(num / 32)
-        num = self.mix(num, div)
-        num = self.prune(num)
+        # Step 2: Divide by 32, mix, and prune
+        num ^= (num >> 5)
+        num %= 16777216
 
-        # Step 3
-        mult: int = num * 2048
-        num = self.mix(num, mult)
-        num = self.prune(num)
+        # Step 3: Multiply by 2048, mix, and prune
+        num ^= (num << 11)
+        num %= 16777216
 
         return num
 
@@ -156,12 +135,12 @@ class Day22:
                 if price_change_seq not in sliding_window_returns:
                     sliding_window_returns[price_change_seq] = {}
 
+                # The buyer sells the first time the change sequence happens
+                if buyer in sliding_window_returns[price_change_seq].keys():
+                    continue
+
                 # The selling price of the previous item in the schedule
                 buying_price = schedule[i+window_size-1][1]
-
-                # The buyer sells the first time the change sequence happens
-                if buyer in sliding_window_returns[price_change_seq]:
-                    continue
 
                 # Track how much each price change sequence gives
                 sliding_window_returns[price_change_seq][buyer] = buying_price
