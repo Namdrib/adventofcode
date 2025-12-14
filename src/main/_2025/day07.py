99f948a4c873fbd48212952e981a951eb0239037
Namdrib <ph0fgazfl4j1@opayq.com>
2025-12-14 15:40:10 +1030

    2025/day07: Re-write with a tree data structure
    Hopefully this makes it easier to do part 2 by doing a recursive descent
    of the tree

--- src/main/_2025/day07.py
+++ src/main/_2025/day07.py
@@ -5,6 +5,19 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from util import helpers
 
+class Node:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
+
+    def __str__(self) -> str:
+        self_str: str = f"({self.x}, {self.y})"
+        left_str: str = f"({self.left.x}, {self.left.y})" if self.left else 'None'
+        right_str: str = f"({self.right.x}, {self.right.y})" if self.right else 'None'
+        return f"{self_str} -> {left_str} and {right_str}"
+
 class Day07:
     """
     Solution for https://adventofcode.com/2025/day/7
@@ -19,6 +32,9 @@ class Day07:
 
         self.start_x: int = None
         self.start_y: int = None
+
+        self.root: Node = None
+        self.nodes: dict = dict()
 
     def read_input(self) -> None:
         """
@@ -45,28 +61,68 @@ class Day07:
                     found_start = True
                     break
 
-    def walk(self, x: int, y: int) -> int:
-        split_count: int = self._walk(x, y, 0)
-        return split_count
+        # Create a tree data structure from the inputs
+        self.create_tree(self.start_x, self.start_y)
 
-    def _walk(self, x: int, y: int, split_count: int) -> int:
+    def create_tree(self, x: int, y: int) -> None:
+        # Find the first node by moving down from the start to the first
+        # splitter
+        new_y: int = y
+        while self.input[new_y][x] != '^':
+            new_y += 1
+
+        # This is where the root of the tree is
+        self.root = Node(x, new_y)
+        self._create_tree(x, new_y, self.root, None)
+
+    def _create_tree(self, x: int, y: int, parent: Node, left: bool) -> Node:
+        # There are no more nodes beyond the end of the grid
         if y >= len(self.grid):
-            return split_count
-
-        # If the current cell is already a tachyon, stop
-        if self.grid[y][x] == '|':
-            return split_count
+            return None
 
         # If the current cell is empty, move down
-        if self.grid[y][x] == '.':
-            self.grid[y][x] = '|'
-            return self._walk(x, y+1, split_count)
+        if self.grid[y][x] in 'S.|':
+            return self._create_tree(x, y+1, parent, left)
 
-        # If the current cell is a splitter, walk the cells to the left and
-        # right
+        # If the current cell is a splitter, create a new node, and set the
+        # parent's child to the new node
+        # We need to know whether we are the left or right child
         if self.grid[y][x] == '^':
-            split_count += 1
-            return self._walk(x-1, y, split_count) + self._walk(x+1, y, 0)
+
+            # Check whether this node has been seen before
+            seen: bool = False
+            if (x, y) in self.nodes.keys():
+                seen = True
+
+            # Create a new node for the current position
+            current = Node(x, y)
+            # If this is the first node we've seen, set the root node
+            if len(self.nodes) == 0:
+                self.root = current
+            self.nodes[(x, y)] = current
+
+            # Set the parent's child
+            if left:
+                parent.left = current
+            else:
+                parent.right = current
+
+            # There is no need to go re-traverse nodes we've visited
+            # Their children have already been recorded
+            # If we do this too early, some nodes might not get their children
+            # set properly
+            if seen:
+                return
+
+            # Walk to the left and right
+            self._create_tree(x-1, y, current, True)
+            self._create_tree(x+1, y, current, False)
+            return
+
+    def count_nodes(self) -> int:
+        # Who needs to recursively walk the tree when we already have all the
+        # nodes :)
+        return len(self.nodes)
 
     def part_one(self) -> int:
         """
@@ -74,7 +130,7 @@ class Day07:
         """
         count: int = 0
 
-        count = self.walk(self.start_x, self.start_y+1)
+        count = self.count_nodes()
 
         return count
 
