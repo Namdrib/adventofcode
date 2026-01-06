416b11d2f82d9b38f7995932c4877237430a6aae
Namdrib <ph0fgazfl4j1@opayq.com>
2026-01-06 23:10:35 +1030

    Add 2025/day09 part 2
    Takes about 6-7 seconds to run

--- src/main/_2025/day09.py
+++ src/main/_2025/day09.py
@@ -17,6 +17,8 @@ class Day09:
         """
         self.input: list = None
         self.tiles: list = None
+        self.x_lines: set = None
+        self.y_lines: set = None
 
     def read_input(self) -> None:
         """
@@ -28,9 +30,22 @@ class Day09:
         self.input = raw_input.splitlines()
 
         self.tiles = []
+        self.x_lines = set()
+        self.y_lines = set()
+
         for item in self.input:
             x, y = [int(x) for x in item.split(',')]
             self.tiles.append((x, y))
+
+        # Build the set of x- and y- lines
+        prev_tile: tuple = self.tiles[-1]
+        for tile in self.tiles:
+            line = (min(tile, prev_tile), max(tile, prev_tile))
+            if tile[0] == prev_tile[0]:
+                self.y_lines.add(line)
+            else:
+                self.x_lines.add(line)
+            prev_tile = tile
 
     def calculate_area_within_tiles(self, tile_1: tuple, tile_2: tuple) -> int:
         x1 = tile_1[0]
@@ -44,6 +59,99 @@ class Day09:
         dy: int = abs(y1 - y2) + 1
 
         return dx * dy
+
+    def line_intersects_rectangle(self, tile_1: tuple, tile_2: tuple, line: tuple) -> bool:
+        """
+        A point is described as a tuple of (x, y)
+        A line is described as a tuple of (point 1, point 2)
+        Assume all lines ahve their co-ordinates sorted. That is:
+        line[0] < line[1] in whatever dimension that matters
+        A rectangle is described as a tuple of (point 1, point 2)
+
+        Return True if a given line intersects the rectangle described by tile_1
+        and tile_2
+
+        :param tile_1: The first tile in a rectangle
+        :type tile_1: tuple
+        :param tile_2: The second tile in a rectangle
+        :type tile_2: tuple
+        :param line: The line to query
+        :type line: tuple
+        :return: True if the line intersects the rectangle, False otherwise
+        :rtype: bool
+        """
+        # Reframe the rectangle in terms of (min x, min y) and (max x, may x)
+        # This makes it easier to refer to the extreme bounds of the rectangle
+        # Also, "shrink" the rectangle in by 1 in each dimension to account for
+        # lines that straddle the boudnary of the rectangle, since each line is also part of the rectangle
+        min_point: tuple = (min(tile_1[0], tile_2[0])+1, min(tile_1[1], tile_2[1])+1)
+        max_point: tuple = (max(tile_1[0], tile_2[0])-1, max(tile_1[1], tile_2[1])-1)
+
+        # Deal with Y lines (vertical)
+        # All the logic here refers to X and Y relationships between a line
+        # along the Y axis
+        # It can be "flipped" to refer to the X axis as well, by swapping all
+        # references to X and Y
+        if line[0][0] == line[1][0]:
+            # A line along the Y axis (that is, vertical) can only intersect the
+            # rectangle if the line's X points are contained within the rectangle's
+            # min X and max X. That is, if it's too far to the left or right, it can
+            # never intersect
+            if line[0][0] < min_point[0] or line[0][0] > max_point[0]:
+                return False
+
+
+            # If the line's X points are contained within the rectangle's min X and
+            # max X, the line can only intersect the rectange if it intersects the
+            # rectangle's min X line, OR it intersects the rectangle's max X line
+            # That is:
+            # The line's min Y <= the rectangle's min Y and the line's max Y >= the rectangle's min Y
+            # The line's min Y <= the rectangle's max Y and the line's max Y >= the rectangle's max Y
+            intersects_min_x_line: bool = line[0][1] <= min_point[1] and line[1][1] >= min_point[1]
+            intersects_max_x_line: bool = line[0][1] <= max_point[1] and line[1][1] >= max_point[1]
+            if intersects_min_x_line or intersects_max_x_line:
+                return True
+            else:
+                return False
+
+        # Deal with X lines (horizontal)
+        else:
+            if line[0][1] < min_point[1] or line[0][1] > max_point[1]:
+                return False
+
+            intersects_min_y_line: bool = line[0][0] <= min_point[0] and line[1][0] >= min_point[0]
+            intersects_max_y_line: bool = line[0][0] <= max_point[0] and line[1][0] >= max_point[0]
+            if intersects_min_y_line or intersects_max_y_line:
+                return True
+            else:
+                return False
+
+            return False
+
+    def region_is_red_or_green_only(self, tile_1: tuple, tile_2: tuple) -> bool:
+        """
+        Return whether the rectangle described by the points at tile_1 and
+        tile_2 would contain only red or green tiles
+
+        :param tile_1: The first tile in a rectangle
+        :type tile_1: tuple
+        :param tile_2: The second tile in a rectangle
+        :type tile_2: tuple
+        :return: True if all the tiles in the rectangle created by tile_1 and
+        tile_2 would contain only red or green tiles, False otherwise
+        :rtype: bool
+        """
+        # If any of the lines intersect, it is not a rectangle
+        for line in self.x_lines:
+            if self.line_intersects_rectangle(tile_1, tile_2, line):
+                return False
+
+        for line in self.y_lines:
+            if self.line_intersects_rectangle(tile_1, tile_2, line):
+                return False
+
+        # If we got to this point, none of the lines intersect
+        return True
 
     def part_one(self) -> int:
         """
@@ -63,11 +171,24 @@ class Day09:
 
     def part_two(self) -> int:
         """
-        Return the ...
+        A tile is green if it exists in the straight line between a red tile and
+        either of the red tiles before or after it
+        That is, the tiles between tiles N-1 and N, and the tiles between tiles
+        N and N+1 are green
+        These green tiles create a polygon. All tiles within this polygon are
+        also green
+        Return the area of the largest rectangle of tiles that can be made from
+        a pair of corner tiles given the entire rectangle must be made of red or
+        green tiles only
         """
-        count: int = 0
+        largest_area: int = 0
 
-        return count
+        for t1, t2 in itertools.combinations(self.tiles, 2):
+            if self.region_is_red_or_green_only(t1, t2):
+                area: int = self.calculate_area_within_tiles(t1, t2)
+                largest_area = max(largest_area, area)
+
+        return largest_area
 
 def main() -> None:
     """
